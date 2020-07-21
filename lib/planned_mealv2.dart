1c200de5e11516e52b904648e8e2371974a2d4f4
iamirzashowvik <showvikmirza@gmail.com>
2020-07-21 18:40:00 +0600

    plannedmealv2

--- lib/planned_mealv2.dart
+++ lib/planned_mealv2.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_widgets/responsive_widgets.dart';
 
---
+++ lib/recipe_details_5.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Recipe extends StatefulWidget {
+  @override
+  _RecipeState createState() => _RecipeState();
+}
+
+class _RecipeState extends State<Recipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
